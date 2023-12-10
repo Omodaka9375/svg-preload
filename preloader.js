4a5b0961807a5ee725d8a91b97f23c2e1dddb13d
Branislav Đalić <branislav.djalic@gmail.com>
2023-12-10 17:29:28 +0100

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -16,7 +16,7 @@ overlay.style.cssText = `
 
 const svgImage = document.createElement('img');
 svgImage.id = 'svgImage';
-svgImage.src = 'https://cdn.jsdelivr.net/gh/omodaka9375/svg-preloader/preload.svg';
+svgImage.src = 'https://cdn.jsdelivr.net/gh/omodaka9375/svg-preload/preload.svg';
 svgImage.style.cssText = `
     max-width: 100%;
     max-height: 100%;
